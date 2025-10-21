747de79d60448e0729fd13c2daf4ea32dc48be5f
wujiabao1 <1145121430@qq.com>
2025-10-21 18:11:04 +0800

    c++

---
+++ hello01.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+int count=0;
+int a,b,c;
+for(a=1;a<=4;a++){
+    for(b=1;b<=4;b++){
+        for(c=1;c<=4;c++){
+            if(a!=b&&a!=c&&b!=c){
+                int num=a*100+b*10+c;
+                printf("%d\n",num);
+                count++;
+                
+            }
+        }
+    }
+}
+}
---
+++ shape.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class Shape {
+public:
+    virtual double area() const = 0;  
+    virtual double volume() const = 0; 
+    virtual ~Shape() {} 
+};
+
+class Cube : public Shape {
+private:
+    double side;
+public:
+    Cube(double s) : side(s) {}
+    
+    
+    double area() const override {
+        return 6 * side * side;
+    }
+    
+
+    double volume() const override {
+        return side * side * side;
+    }
+};
+
+class Sphere: public Shape {
+private:
+    double radius;
+public:   
+    Sphere(double r) : radius(r) {}
+    
+    
+    double area() const override {
+        return 4 * M_PI * radius * radius;
+    }
+    
+    double volume() const override {
+        return (4.0/3.0) * M_PI * radius * radius * radius;
+    }
+};
+
+int main() {
+    double value;
+    cin >> value;
+    
+   
+    Shape* cube = new Cube(value);
+    Shape* sphere = new Sphere(value);
+    
+   
+    cout << cube->volume() << " " 
+         << cube->area() << " " 
+         << sphere->volume() << " " 
+         << sphere->area() << endl;
+    
+ 
+    delete cube;
+    delete sphere;
+    
+    return 0;
+}
